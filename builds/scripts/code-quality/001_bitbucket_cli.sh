dd05226aa340d0e2d141f767585ba6334814fc93
Chris Lee <chrisl@cnyes.com>
2018-06-01 14:29:59 +0800

    workaround for docker exec missing option -w with jenkins slave docker version.

--- builds/scripts/code-quality/001_bitbucket_cli.sh
+++ builds/scripts/code-quality/001_bitbucket_cli.sh
@@ -2,7 +2,7 @@
 
 TEST_APP_ID="$(get_container_id "${CMD_NAME_DEV}")"
 
-"${DOCKER_EXEC_CMD[@]}" -w /srv/app/src/bitbucket-cli "${TEST_APP_ID}" composer run-script ci:test || ERROR="true"
+"${DOCKER_EXEC_CMD[@]}" "${TEST_APP_ID}" sh -c 'cd /srv/app/src/bitbucket-cli && composer run-script ci:test' || ERROR="true"
 [[ ! -d "${PARENT_DIR}/builds/reports" ]] && mkdir -p "${PARENT_DIR}/builds/reports"
 docker cp "${TEST_APP_ID}":/srv/app/builds/reports/bitbucket-cli "${PARENT_DIR}/builds/reports/"
 
